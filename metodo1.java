fe90d9cfad8abbf47df053f827f4b3f6c14dc814
HenryGV <162t0306@itsm.edu.mx>
2017-02-16 23:43:16 -0600

    Delete metodo1.java

--- metodo1.java
+++
@@ -1,9 +0,0 @@
-public static float media (float n1, float n2)
-{
-teturn (n1+n2)/2;
-}
-public static void main (String[] args)
-{
-float resultado = media (1,2);
-System.out.println("Media = " + resultado);
-}
